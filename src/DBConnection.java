96bb3c356b3c1baf78e7175fd8769e80cb422c7a
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-11 23:21:05 +0200

    Postgres credentials

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,3 +1,11 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/HospitalManagement";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Eustache1998";
+
+}
